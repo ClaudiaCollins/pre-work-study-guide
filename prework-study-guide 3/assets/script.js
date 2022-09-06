96b3f899ef11a460af428020ae1ff5d91173ff3c
Claudia Collins <claudiaryannc@gmail.com>
2022-09-06 14:59:59 -0700

    fixed bugs in javascript

--- prework-study-guide 3/assets/script.js
+++ prework-study-guide 3/assets/script.js
@@ -1,19 +1,28 @@
-var topic = "CSS";
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
-}
-
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  } 
+  function selectTopic() {
+    if (topics[0] === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (randomTopic] === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+  }
 var shapes = ["triangle", "square", "pentagon", "circle"];
 console.log(shapes[0]);
-
 var shapes = ["triangle", "square", "pentagon", "circle"];
-console.log(shapes[0]);                                                                                                                                              
+console.log(shapes[0]);  
+listTopics()                                                                                                                 
+selectTopic()
